af78e5f5336932bfb3fd6b6493c74bec56587b94
ssuhan1 <87295315+ssuhan1@users.noreply.github.com>
2021-11-08 02:19:12 -0500

    Add files via upload

---
+++ FFMLoginTest.java
@@ -0,0 +1,30 @@
+package first_test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FFMLoginTest {
+	
+	 static WebDriver driver;
+	 static String baseURL="http://143.198.127.220/";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sadma\\Downloads\\ChromeDriver\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get(baseURL);
+		
+		driver.findElement(By.id("login")).sendKeys("carpediem@example.com");
+		driver.findElement(By.id("password")).sendKeys("carpediem");
+		driver.findElement(By.xpath("//*[@id=\"formContent\"]/form/input[3]")).click();
+		
+		
+
+	}
+
+}
